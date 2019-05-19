68153f7dfec77f32f22a908a959bb4c9d0fbec46
xenahort <juan.carlos.wow.95@gmail.com>
2019-05-19 12:04:52 +0200

    Probando ng-repeat

--- 2 Hola controller/controller.js
+++ 2 Hola controller/controller.js
@@ -1,4 +1,22 @@
 var app = angular.module("MyFirstApp",[])
 app.controller("FirstController", function($scope){
   $scope.nombre = "JC";
+  $scope.comentarios=[
+    {
+      comentario: "Soy el comentario 1",
+      usuario: "Usuario 1"
+    },
+    {
+      comentario: "Soy el comentario 2",
+      usuario: "Usuario 2"
+    },
+    {
+      comentario: "Soy el comentario 3",
+      usuario: "Usuario 3"
+    },
+    {
+      comentario: "Soy el comentario 4",
+      usuario: "Usuario 4"
+    }
+  ];
 });
